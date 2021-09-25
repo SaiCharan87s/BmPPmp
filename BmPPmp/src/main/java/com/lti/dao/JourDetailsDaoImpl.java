cd135768384cab010f218766c0eb3a1ffabae6ea
SaiCharan87s <pickraju24@gmail.com>
2021-09-25 19:30:48 +0530

    this is journey commit

--- BmPPmp/src/main/java/com/lti/dao/JourDetailsDaoImpl.java
+++ BmPPmp/src/main/java/com/lti/dao/JourDetailsDaoImpl.java
@@ -1,28 +1,29 @@
-//package com.lti.dao;
-//
-//public class JourDetailsDaoImpl {
-//
-//	import java.security.Timestamp;
-//	import java.sql.Date;
-//	import java.util.List;
-//
-//	import javax.persistence.EntityManager;
-//	import javax.persistence.Query;
-//
-//	import org.springframework.stereotype.Repository;
-//	import org.springframework.transaction.annotation.Transactional;
-//
-//	import com.example.demo.layer2.BusDetails;
-//	import com.example.demo.layer2.BusJourneyDetails;
-//	import com.example.demo.layer2.BusJourneyDetailsNotFoundException;
-//	@Repository
-//	public class BusJourneyDetailsRepositoryImpl extends BaseRepository implements BusJourneyDetailsRepository{
-//
-//		
-//		public class JourDetailsDaoImpl {
-//		{
-//			System.out.println("BusJourneyDetailsRepositoryImpl()..Created...");
-//		}
+package com.lti.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.lti.model.JourDetails;
+import com.lti.model.JourDetailsException;
+
+@Repository
+public class JourDetailsDaoImpl implements JourDetailsDao {
+
+	@PersistenceContext
+	EntityManager entityManager;
+
+	public JourDetailsDaoImpl() 
+
+		{
+			System.out.println("BusJourneyDetailsRepositoryImpl()..Created...");
+		}
+
 //		@Transactional
 //		public List<BusJourneyDetails> selectAllBusJourneyDetails() {
 //			// TODO Auto-generated method stub
@@ -30,8 +31,8 @@
 //
 //			Query query = entityManager.createQuery("from BusJourneyDetails");
 //			List<BusJourneyDetails> busJourneyDetailsList = query.getResultList();
-//			System.out.println("busDetailsList "+busJourneyDetailsList.size());
-//			
+//			System.out.println("busDetailsList " + busJourneyDetailsList.size());
+//
 //			return busJourneyDetailsList;
 //		}
 //
@@ -40,17 +41,18 @@
 //			// TODO Auto-generated method stub
 //			System.out.println("inside selectbusjourney...");
 //			EntityManager entityManager = getEntityManager();
-//			return entityManager.find(BusJourneyDetails.class, journey_id);//based on PK
-//			
+//			return entityManager.find(BusJourneyDetails.class, journey_id);// based on PK
+//
 //		}
 //
 //		@Transactional
 //		public List<BusJourneyDetails> selectBusJourneyDetailsbyDepartureTime(String departure_Time)
 //				throws BusJourneyDetailsNotFoundException {
 //			EntityManager entityManager = getEntityManager();
-//			Query query = entityManager.createQuery("select j from BusJourneyDetails j where j.departure_Time = :vdeparture_Time");
+//			Query query = entityManager
+//					.createQuery("select j from BusJourneyDetails j where j.departure_Time = :vdeparture_Time");
 //			query.setParameter("vdeparture_Time", departure_Time);
-//			List<BusJourneyDetails>  busJourneyDetailsbyDepartureTime= query.getResultList();
+//			List<BusJourneyDetails> busJourneyDetailsbyDepartureTime = query.getResultList();
 //			return busJourneyDetailsbyDepartureTime;
 //		}
 //
@@ -59,9 +61,10 @@
 //				throws BusJourneyDetailsNotFoundException {
 //			// TODO Auto-generated method stub
 //			EntityManager entityManager = getEntityManager();
-//			Query query = entityManager.createQuery("select j from BusJourneyDetails j where j.arrival_Time = :varrival_Time");
+//			Query query = entityManager
+//					.createQuery("select j from BusJourneyDetails j where j.arrival_Time = :varrival_Time");
 //			query.setParameter("varrival_Time", arrival_Time);
-//			List<BusJourneyDetails>  busJourneyDetailsbyArrivalTime= query.getResultList();
+//			List<BusJourneyDetails> busJourneyDetailsbyArrivalTime = query.getResultList();
 //			return busJourneyDetailsbyArrivalTime;
 //		}
 //
@@ -70,9 +73,10 @@
 //				throws BusJourneyDetailsNotFoundException {
 //			// TODO Auto-generated method stub
 //			EntityManager entityManager = getEntityManager();
-//			Query query = entityManager.createQuery("select j from BusJourneyDetails j where j.source_loc = :vsource_loc");
+//			Query query = entityManager
+//					.createQuery("select j from BusJourneyDetails j where j.source_loc = :vsource_loc");
 //			query.setParameter("vsource_loc", source_loc);
-//			List<BusJourneyDetails>  busJourneyDetailsbysource_loc= query.getResultList();
+//			List<BusJourneyDetails> busJourneyDetailsbysource_loc = query.getResultList();
 //			return busJourneyDetailsbysource_loc;
 //		}
 //
@@ -81,9 +85,10 @@
 //				throws BusJourneyDetailsNotFoundException {
 //			// TODO Auto-generated method stub
 //			EntityManager entityManager = getEntityManager();
-//			Query query = entityManager.createQuery("select j from BusJourneyDetails j where j.destination_loc = :vdestination_loc");
+//			Query query = entityManager
+//					.createQuery("select j from BusJourneyDetails j where j.destination_loc = :vdestination_loc");
 //			query.setParameter("vdestination_loc", destination_loc);
-//			List<BusJourneyDetails>  busJourneyDetailsbydestinationLoc= query.getResultList();
+//			List<BusJourneyDetails> busJourneyDetailsbydestinationLoc = query.getResultList();
 //			return busJourneyDetailsbydestinationLoc;
 //		}
 //
@@ -92,10 +97,11 @@
 //				throws BusJourneyDetailsNotFoundException {
 //			// TODO Auto-generated method stub
 //			EntityManager entityManager = getEntityManager();
-//			Query query = entityManager.createQuery("select j from BusJourneyDetails j where j.departure_date = :vdeparture_date");
+//			Query query = entityManager
+//					.createQuery("select j from BusJourneyDetails j where j.departure_date = :vdeparture_date");
 //			query.setParameter("vdeparture_date", departure_date);
-//			List<BusJourneyDetails>  busJourneyDetailsbydeparture_date= query.getResultList();
-//			return  busJourneyDetailsbydeparture_date;
+//			List<BusJourneyDetails> busJourneyDetailsbydeparture_date = query.getResultList();
+//			return busJourneyDetailsbydeparture_date;
 //		}
 //
 //		@Transactional
@@ -103,25 +109,27 @@
 //				throws BusJourneyDetailsNotFoundException {
 //			// TODO Auto-generated method stub
 //			EntityManager entityManager = getEntityManager();
-//			Query query = entityManager.createQuery("select j from BusJourneyDetails j where j.journeyID = :vjourneyID");
+//			Query query = entityManager
+//					.createQuery("select j from BusJourneyDetails j where j.journeyID = :vjourneyID");
 //			query.setParameter("vjourneyID", journeyID);
-//			List<BusJourneyDetails>  busJourneyDetailsbyjourneyID= query.getResultList();
-//			return  busJourneyDetailsbyjourneyID;
+//			List<BusJourneyDetails> busJourneyDetailsbyjourneyID = query.getResultList();
+//			return busJourneyDetailsbyjourneyID;
 //		}
 //
 //		@Transactional
 //		public int selectAvailableSeates(String busNo, int journeyId) throws BusJourneyDetailsNotFoundException {
 //			// TODO Auto-generated method stub
 //			EntityManager entityManager = getEntityManager();
-//			Query query = entityManager.createQuery("select b from BusJourneyDetails b where b.bus_no = : vbus_no and b.journey_ID = : v_journeyID");
+//			Query query = entityManager.createQuery(
+//					"select b from BusJourneyDetails b where b.bus_no = : vbus_no and b.journey_ID = : v_journeyID");
 //			query.setParameter("vbus_no", busNo);
 //			query.setParameter("v_journeyID", journeyId);
-//			int seates=  query.getFirstResult();
-//			System.out.println("Seates availabel "+seates);
-//			return seates ;
+//			int seates = query.getFirstResult();
+//			System.out.println("Seates availabel " + seates);
+//			return seates;
 //		}
 //
-//	//@Transactional 
+//		// @Transactional
 ////		public boolean updatebusRoute(int journeyId, String source, String destination) {
 ////			// TODO Auto-generated method stub
 ////			boolean res=false;
@@ -138,24 +146,96 @@
 ////		}
 //		@Transactional
 //		public void decreaseSeatCount(int jID, int rID) {
-//		EntityManager entityManager = getEntityManager();
-//		Query query = entityManager.createQuery("update BusJourneyDetails b set b.available_seates = ((select s.available_seates from BusJourneyDetails s where s.journeyID = :jID) - (select r.seats_Booked from ReservationDetails r where r.journey_Id = :jID and r.reservation_id = :rID)) where b.journeyID = :jID").setParameter("jID",jID).setParameter("rID", rID);
-//		query.executeUpdate();
+//			EntityManager entityManager = getEntityManager();
+//			Query query = entityManager.createQuery(
+//					"update BusJourneyDetails b set b.available_seates = ((select s.available_seates from BusJourneyDetails s where s.journeyID = :jID) - (select r.seats_Booked from ReservationDetails r where r.journey_Id = :jID and r.reservation_id = :rID)) where b.journeyID = :jID")
+//					.setParameter("jID", jID).setParameter("rID", rID);
+//			query.executeUpdate();
 //		}
-//		
-//		
-//		@Transactional
-//		public void changeAvailableSeats(int jID, int rID) throws NumberNotFoundException{
-//		EntityManager entityManager = getEntityManager();
-//		Query query = entityManager.createQuery("update BusJourneyDetails b set b.available_seates = (select r.seats_Booked from ReservationDetails r where r.journey_Id = :jID and r.reservation_id = :rID) + b.available_seates where b.journeyID = :jID").setParameter("jID",jID).setParameter("rID", rID);
-//		query.executeUpdate();
-//		}
-//		
-//		
-//		
-//		
-//		
-//		
-//	}
 //
-//}
+//		@Transactional
+//		public void changeAvailableSeats(int jID, int rID) throws NumberNotFoundException {
+//			EntityManager entityManager = getEntityManager();
+//			Query query = entityManager.createQuery(
+//					"update BusJourneyDetails b set b.available_seates = (select r.seats_Booked from ReservationDetails r where r.journey_Id = :jID and r.reservation_id = :rID) + b.available_seates where b.journeyID = :jID")
+//					.setParameter("jID", jID).setParameter("rID", rID);
+//			query.executeUpdate();
+//		}
+
+	@Override
+	@Transactional
+	public List<JourDetails> selectAllBusJourneyDetails() {
+		// TODO Auto-generated method stub
+		Query query = entityManager.createQuery("from BusJourneyDetails");
+		List<JourDetails> busJourneyDetailsList = query.getResultList();
+		System.out.println("busDetailsList " + busJourneyDetailsList.size());
+		return busJourneyDetailsList;
+	}
+
+	@Override
+	@Transactional
+	public JourDetails selectBusJourneyDetails(int journey_id) throws JourDetailsException {
+		// TODO Auto-generated method stub
+		System.out.println("inside selectbusjourney...");
+		return entityManager.find(JourDetails.class, journey_id);
+	}
+
+	@Override
+	@Transactional
+	public List<JourDetails> selectBusJourneyDetailsbySourceLocation(String sourceLoc)
+		throws JourDetailsException {
+
+			Query query = entityManager.createQuery("select j from BusJourneyDetails j where j.source_loc = :vsource_loc");
+			query.setParameter("vsource_loc", sourceLoc);
+			List<JourDetails>  busJourneyDetailsbysource_loc= query.getResultList();
+			return busJourneyDetailsbysource_loc;
+	}
+
+	@Override
+	@Transactional
+	public List<JourDetails> selectBusJourneyDetailsbyDestinationLocation(String destinationLoc)
+			throws JourDetailsException {
+		Query query = entityManager.createQuery("select j from BusJourneyDetails j where j.destination_loc = :vdestination_loc");
+		query.setParameter("vdestination_loc", destinationLoc);
+		List<JourDetails>  busJourneyDetailsbydestinationLoc= query.getResultList();
+		return busJourneyDetailsbydestinationLoc;
+	}
+	
+	
+	@Override
+	@Transactional
+	public List<JourDetails> selectBusJourneyDetailsbyJourneyID(int journeyId) throws JourDetailsException {
+		Query query = entityManager.createQuery("select j from BusJourneyDetails j where j.journeyID = :vjourneyID");
+		query.setParameter("vjourneyID", journeyId);
+		List<JourDetails>  busJourneyDetailsbyjourneyID= query.getResultList();
+		return  busJourneyDetailsbyjourneyID;
+	}
+
+	@Override
+	@Transactional
+	public int selectAvailableSeates(String busNo, int journeyId) throws JourDetailsException {
+		Query query = entityManager.createQuery("select b from BusJourneyDetails b where b.bus_no = : vbus_no and b.journey_ID = : v_journeyID");
+		query.setParameter("vbus_no", busNo);
+		query.setParameter("v_journeyID", journeyId);
+		int seates=  query.getFirstResult();
+		System.out.println("Seates availabel "+seates);
+		return seates ;
+	}
+
+	@Override
+	@Transactional
+	public void changeAvailableSeats(int jID, int rID) throws NumberException {
+		Query query = entityManager.createQuery("update BusJourneyDetails b set b.available_seates = ((select s.available_seates from BusJourneyDetails s where s.journeyID = :jID) - (select r.seats_Booked from ReservationDetails r where r.journey_Id = :jID and r.reservation_id = :rID)) where b.journeyID = :jID").setParameter("jID",jID).setParameter("rID", rID);
+		query.executeUpdate();
+		}
+		
+
+	@Override
+	@Transactional
+	public void decreaseSeatCount(int jID, int rID) {
+		Query query = entityManager.createQuery("update BusJourneyDetails b set b.available_seates = (select r.seats_Booked from ReservationDetails r where r.journey_Id = :jID and r.reservation_id = :rID) + b.available_seates where b.journeyID = :jID").setParameter("jID",jID).setParameter("rID", rID);
+		query.executeUpdate();
+		
+	}
+
+}
